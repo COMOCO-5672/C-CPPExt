12a7af991ead467d308fb740abb91816f419e9ce
comoco-5672 <775324623@qq.com>
2023-11-07 22:45:58 +0800

    文件格式化

--- RAII/auto_ptr.hpp
+++ RAII/auto_ptr.hpp
@@ -1,22 +1,18 @@
 
 // can use ?
 
-template <class T>
-class auto_ptr
-{
-private:
+template <class T> class auto_ptr {
+  private:
     /* data */
 
-public:
+  public:
     explicit auto_ptr(T *p = 0);
 
-    template <class U>
-    auto_ptr(auto_ptr<U> &rhs);
+    template <class U> auto_ptr(auto_ptr<U> &rhs);
 
     ~auto_ptr();
 
-    template <class U>
-    auto_ptr<T> &operator=(auto_ptr<U> &rhs);
+    template <class U> auto_ptr<T> &operator=(auto_ptr<U> &rhs);
 
     T &operator*() const;
     T *operator->() const;
@@ -24,65 +20,43 @@ public:
     T *release() const;
     void reset(T *p = 0);
 
-private:
+  private:
     T *pointee;
     friend class auto_ptr<U>;
 };
 
-template <class T>
-inline auto_ptr<T>::auto_ptr(T *p) : pointee(p)
-{
-}
+template <class T> inline auto_ptr<T>::auto_ptr(T *p) : pointee(p) {}
 
 template <class T>
 template <class U>
-inline auto_ptr<T>::auto_ptr(auto_ptr<U> &rhs) : pointee(rhs.release()) {}
-
-template <class T>
-inline auto_ptr<T>::~auto_ptr()
+inline auto_ptr<T>::auto_ptr(auto_ptr<U> &rhs) : pointee(rhs.release())
 {
-    delete pointee;
 }
 
-template <class T>
-template <class U>
-inline auto_ptr<T> &auto_ptr<T>::operator=(auto_ptr<U> &rhs)
+template <class T> inline auto_ptr<T>::~auto_ptr() { delete pointee; }
+
+template <class T> template <class U> inline auto_ptr<T> &auto_ptr<T>::operator=(auto_ptr<U> &rhs)
 {
     if (this != rhs)
         reset(rhs.release());
     return *this;
 }
-template <class T>
-inline T &auto_ptr<T>::operator*() const
-{
-    return *pointee;
-}
+template <class T> inline T &auto_ptr<T>::operator*() const { return *pointee; }
 
-template <class T>
-inline T *auto_ptr<T>::operator->() const
-{
-    return pointee;
-}
+template <class T> inline T *auto_ptr<T>::operator->() const { return pointee; }
 
-template <class T>
-inline T *auto_ptr<T>::get() const
-{
-    return pointee;
-}
+template <class T> inline T *auto_ptr<T>::get() const { return pointee; }
 
-template <class T>
-inline T *auto_ptr<T>::release()
+template <class T> inline T *auto_ptr<T>::release()
 {
     T *oldPointee = pointee;
     pointee = 0;
     return oldPointee;
 }
 
-template <class T>
-inline void auto_ptr<T>::reset(T *p)
+template <class T> inline void auto_ptr<T>::reset(T *p)
 {
-    if (pointee != p)
-    {
+    if (pointee != p) {
         delete pointee;
         pointee = p;
     }
--- RAII/threadsafe_stack.hpp
+++ RAII/threadsafe_stack.hpp
@@ -1,40 +1,36 @@
-#include <stack>
-#include <mutex>
 #include <exception>
+#include <mutex>
+#include <stack>
 
-struct empty_stack:std::exception
-{
-    const char* what() const throw();
+struct empty_stack : std::exception {
+    const char *what() const throw();
 };
 
-template<class T>
-class threadsafe_stack
-{
-private:
-
+template <class T> class threadsafe_stack {
+  private:
     std::stack<T> m_stack_;
     std::mutex m_mutex_
 
-public:
-    threadsafe_stack() {};
-    threadsafe_stack(const threadsafe_stack& other)
+        public : threadsafe_stack() {};
+    threadsafe_stack(const threadsafe_stack &other)
     {
         std::lock_guard<std::mutex> lock(other.m_mutex_);
         m_stack_ = other.m_stack_;
     }
 
-    ~threadsafe_stack() {
+    ~threadsafe_stack()
+    {
         std::lock_guard<std::mutex> lock(m_mutex_);
         std::stack<T> s;
         m_stack_.swap(s);
     };
 
-    threadsafe_stack& operator=(const threadsafe_stack&) = delete;
+    threadsafe_stack &operator=(const threadsafe_stack &) = delete;
 
     std::shared_ptr<T> pop()
     {
         std::lock_guard<std::mutex> lock(m_mutex_);
-        if(m_stack_.empty())
+        if (m_stack_.empty())
             throw empty_stack();
 
         std::shared_ptr<T> const res(std::make_shared<T>(m_stack_.top()));
@@ -63,6 +59,4 @@ public:
         std::lock_guard<std::mutex> lock(m_mutex_);
         return m_stack_.empty();
     }
-
 };
-
